bc3c5ad49122ec36db5334f1e97e05c9d953973f
sanika-chavan07 <sanikachavan4836@gmail.com>
2026-06-08 12:21:46 +0530

    work on

--- src/app/shared/component/get-confirm/get-confirm.component.ts
+++ src/app/shared/component/get-confirm/get-confirm.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-get-confirm',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./get-confirm.component.scss']
 })
 export class GetConfirmComponent implements OnInit {
-
-  constructor() { }
+  msg !: string
+  constructor(
+    @Inject(MAT_DIALOG_DATA) data: string,
+    private _matDialogRef: MatDialogRef<GetConfirmComponent>
+  ) {
+    this.msg = data
+  }
 
   ngOnInit(): void {
   }
 
+  onClose(flag: boolean) {
+    this._matDialogRef.close(flag)
+  }
 }
